f00b10613fe9adbcdcb55faf547e7474d80bd79d
Agustin <abersit270@g.educaand.es>
2025-11-24 10:39:56 +0100

    Colocado Links y splash

--- Relacion_de_ejercicios_de_Flutter/ejercicios/lib/screens/ejercicio1.dart
+++ Relacion_de_ejercicios_de_Flutter/ejercicios/lib/screens/ejercicio1.dart
@@ -1,7 +1,12 @@
 import 'package:ejercicios/utils/app_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Ejercicio1 extends StatelessWidget {
+  final Uri _repoUrl = Uri.parse(
+    "https://github.com/abersit270/PMDM25-26-Agustin-Nahuel-Bernal-Sita.git",
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,12 +15,24 @@ class Ejercicio1 extends StatelessWidget {
       body: Column(
         children: [
           Text(
-            'Nombre: Agustín Nahuel Bernal Sita',
-            style: TextStyle(fontSize: 30, fontFamily: "Courier New"),
+            "Mi nombre es Agustín Nahuel Bernal y estoy aprendiendo flutter",
+            style: TextStyle(fontSize: 20, fontFamily: "Courier New"),
           ),
-          Text(
-            'Dirección del repositorio: https://github.com/abersit270/PMDM25-26-Agustin-Nahuel-Bernal-Sita.git',
-            style: TextStyle(fontSize: 30, fontFamily: "Courier New"),
+          InkWell(
+            onTap: () async {
+              if (await canLaunchUrl(_repoUrl)) {
+                await launchUrl(_repoUrl, mode: LaunchMode.externalApplication);
+              }
+            },
+            child: Text(
+              'Repositorio en GitHub',
+              style: TextStyle(
+                fontSize: 30,
+                fontFamily: "Courier New",
+                color: Colors.blue,
+                decoration: TextDecoration.underline,
+              ),
+            ),
           ),
         ],
       ),
